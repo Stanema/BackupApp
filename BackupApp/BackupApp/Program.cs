da95f475eca6fb56bec01e51e6b1744bb7f8a558
Staněk, Martin <stanema7@fbmi.cvut.cz>
2023-12-05 18:33:21 +0100

    init

---
+++ BackupApp/BackupApp/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+
+
+
+
+Console.WriteLine("Hello, World!");
+
+void LoadJson()
+{
+using (StreamReader r = new StreamReader("file.json"))
+{
+string json = r.ReadToEnd();
+List<Config> items = JsonConvert.DeserializeObject<List<Config>>(json);
+}
+}
+
+public class Config
+{
+    public List<string> sources;
+    public List<string> targets;
+    public DateTime timing;
+    public string methods;
+    public List<string> retention;
+}
